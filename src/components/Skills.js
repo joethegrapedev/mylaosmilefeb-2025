b3cd01eb0e8dc8f1c05654d2fe6f6d6c10fa7ff2
John <johntowzhichongdev@gmail.com>
2025-02-17 08:41:44 +0800

    ran git build

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,122 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { motion } from "framer-motion";
+import { jsx as _jsx } from "react/jsx-runtime";
 const Skills = () => {
-    return (_jsxs(motion.div, { initial: { opacity: 0 }, animate: { opacity: 1, transition: { duration: 0.5 } }, className: "w-full flex flex-col lgl:flex-row gap-10 lgl:gap-20", children: [_jsxs("div", { className: "w-full lgl:w-1/2", children: [_jsxs("div", { className: "py-12 font-titleFont flex flex-col gap-4", children: [_jsx("p", { className: "text-sm text-designColor tracking-[4px] uppercase", children: "Features" }), _jsx("h2", { className: "text-3xl md:text-4xl font-bold", children: "Design Skill" })] }), _jsxs("div", { className: 'className="mt-14 w-full flex flex-col gap-6', children: [_jsxs("div", { className: "overflow-x-hidden", children: [_jsx("p", { className: "text-sm uppercase font-medium", children: "Photoshot" }), _jsx("span", { className: "w-full h-2 bgOpacity rounded-md inline-flex mt-2", children: _jsx(motion.span, { initial: { x: "-100%", opacity: 0 }, animate: { x: 0, opacity: 1 }, transition: { duration: 0.5, delay: 0.5 }, className: "w-full h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative", children: _jsx("span", { className: "absolute -top-7 right-0", children: "100%" }) }) })] }), _jsxs("div", { className: "overflow-x-hidden", children: [_jsx("p", { className: "text-sm uppercase font-medium", children: "Figma" }), _jsx("span", { className: "w-full h-2 bgOpacity rounded-md inline-flex mt-2", children: _jsx(motion.span, { initial: { x: "-100%", opacity: 0 }, animate: { x: 0, opacity: 1 }, transition: { duration: 0.5, delay: 0.5 }, className: "w-[90%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative", children: _jsx("span", { className: "absolute -top-7 right-0", children: "90%" }) }) })] }), _jsxs("div", { className: "overflow-x-hidden", children: [_jsx("p", { className: "text-sm uppercase font-medium", children: "Adobe XD." }), _jsx("span", { className: "w-full h-2 bgOpacity rounded-md inline-flex mt-2", children: _jsx(motion.span, { initial: { x: "-100%", opacity: 0 }, animate: { x: 0, opacity: 1 }, transition: { duration: 0.5, delay: 0.5 }, className: "w-[60%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative", children: _jsx("span", { className: "absolute -top-7 right-0", children: "60%" }) }) })] }), _jsxs("div", { className: "overflow-x-hidden", children: [_jsx("p", { className: "text-sm uppercase font-medium", children: "Adobe Illustrator" }), _jsx("span", { className: "w-full h-2 bgOpacity rounded-md inline-flex mt-2", children: _jsx(motion.span, { initial: { x: "-100%", opacity: 0 }, animate: { x: 0, opacity: 1 }, transition: { duration: 0.5, delay: 0.5 }, className: "w-[70%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative", children: _jsx("span", { className: "absolute -top-7 right-0", children: "70%" }) }) })] }), _jsxs("div", { className: "overflow-x-hidden", children: [_jsx("p", { className: "text-sm uppercase font-medium", children: "Design" }), _jsx("span", { className: "w-full h-2 bgOpacity rounded-md inline-flex mt-2", children: _jsx(motion.span, { initial: { x: "-100%", opacity: 0 }, animate: { x: 0, opacity: 1 }, transition: { duration: 0.5, delay: 0.5 }, className: "w-[95%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative", children: _jsx("span", { className: "absolute -top-7 right-0", children: "95%" }) }) })] })] })] }), _jsxs("div", { className: "w-full lgl:w-1/2", children: [_jsxs("div", { className: "py-12 font-titleFont flex flex-col gap-4", children: [_jsx("p", { className: "text-sm text-designColor tracking-[4px] uppercase", children: "Features" }), _jsx("h2", { className: "text-3xl md:text-4xl font-bold", children: "Development Skill" })] }), _jsxs("div", { className: "flex flex-col gap-6", children: [_jsxs("div", { className: "overflow-x-hidden", children: [_jsx("p", { className: "text-sm uppercase font-medium", children: "React" }), _jsx("span", { className: "w-full h-2 bgOpacity rounded-md inline-flex mt-2", children: _jsx(motion.span, { initial: { x: "-100%", opacity: 0 }, animate: { x: 0, opacity: 1 }, transition: { duration: 0.5, delay: 0.5 }, className: "w-full h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative", children: _jsx("span", { className: "absolute -top-7 right-0", children: "100%" }) }) })] }), _jsxs("div", { className: "overflow-x-hidden", children: [_jsx("p", { className: "text-sm uppercase font-medium", children: "HTML 5" }), _jsx("span", { className: "w-full h-2 bgOpacity rounded-md inline-flex mt-2", children: _jsx(motion.span, { initial: { x: "-100%", opacity: 0 }, animate: { x: 0, opacity: 1 }, transition: { duration: 0.5, delay: 0.5 }, className: "w-[95%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative", children: _jsx("span", { className: "absolute -top-7 right-0", children: "95%" }) }) })] }), _jsxs("div", { className: "overflow-x-hidden", children: [_jsx("p", { className: "text-sm uppercase font-medium", children: "CSS3" }), _jsx("span", { className: "w-full h-2 bgOpacity rounded-md inline-flex mt-2", children: _jsx(motion.span, { initial: { x: "-100%", opacity: 0 }, animate: { x: 0, opacity: 1 }, transition: { duration: 0.5, delay: 0.5 }, className: "w-[80%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative", children: _jsx("span", { className: "absolute -top-7 right-0", children: "80%" }) }) })] }), _jsxs("div", { className: "overflow-x-hidden", children: [_jsx("p", { className: "text-sm uppercase font-medium", children: "JAVASCRIPT" }), _jsx("span", { className: "w-full h-2 bgOpacity rounded-md inline-flex mt-2", children: _jsx(motion.span, { initial: { x: "-100%", opacity: 0 }, animate: { x: 0, opacity: 1 }, transition: { duration: 0.5, delay: 0.5 }, className: "w-[75%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative", children: _jsx("span", { className: "absolute -top-7 right-0", children: "75%" }) }) })] }), _jsxs("div", { className: "overflow-x-hidden", children: [_jsx("p", { className: "text-sm uppercase font-medium", children: "SOFTWARE" }), _jsx("span", { className: "w-full h-2 bgOpacity rounded-md inline-flex mt-2", children: _jsx(motion.span, { initial: { x: "-100%", opacity: 0 }, animate: { x: 0, opacity: 1 }, transition: { duration: 0.5, delay: 0.5 }, className: "w-[90%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative", children: _jsx("span", { className: "absolute -top-7 right-0", children: "90%" }) }) })] })] })] })] }));
+    return (_jsx("div", {})
+    // <motion.div
+    //   initial={{ opacity: 0 }}
+    //   animate={{ opacity: 1, transition: { duration: 0.5 } }}
+    //   className="w-full flex flex-col lgl:flex-row gap-10 lgl:gap-20"
+    // >
+    //   <div className="w-full lgl:w-1/2">
+    //     <div className="py-12 font-titleFont flex flex-col gap-4">
+    //       <p className="text-sm text-designColor tracking-[4px] uppercase">
+    //         Features
+    //       </p>
+    //       <h2 className="text-3xl md:text-4xl font-bold">Design Skill</h2>
+    //     </div>
+    //     <div className='className="mt-14 w-full flex flex-col gap-6'>
+    //       <div className="overflow-x-hidden">
+    //         <p className="text-sm uppercase font-medium">Photoshot</p>
+    //         <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
+    //           <motion.span
+    //             initial={{ x: "-100%", opacity: 0 }}
+    //             animate={{ x: 0, opacity: 1 }}
+    //             transition={{ duration: 0.5, delay: 0.5 }}
+    //             className="w-full h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
+    //           >
+    //             <span className="absolute -top-7 right-0">100%</span>
+    //           </motion.span>
+    //         </span>
+    //       </div>
+    //       <div className="overflow-x-hidden">
+    //         <p className="text-sm uppercase font-medium">Figma</p>
+    //         <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
+    //           <motion.span
+    //             initial={{ x: "-100%", opacity: 0 }}
+    //             animate={{ x: 0, opacity: 1 }}
+    //             transition={{ duration: 0.5, delay: 0.5 }}
+    //             className="w-[90%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
+    //           >
+    //             <span className="absolute -top-7 right-0">90%</span>
+    //           </motion.span>
+    //         </span>
+    //       </div>
+    //       <div className="overflow-x-hidden">
+    //         <p className="text-sm uppercase font-medium">Adobe XD.</p>
+    //         <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
+    //           <motion.span
+    //             initial={{ x: "-100%", opacity: 0 }}
+    //             animate={{ x: 0, opacity: 1 }}
+    //             transition={{ duration: 0.5, delay: 0.5 }}
+    //             className="w-[60%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
+    //           >
+    //             <span className="absolute -top-7 right-0">60%</span>
+    //           </motion.span>
+    //         </span>
+    //       </div>
+    //       <div className="overflow-x-hidden">
+    //         <p className="text-sm uppercase font-medium">Adobe Illustrator</p>
+    //         <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
+    //           <motion.span
+    //             initial={{ x: "-100%", opacity: 0 }}
+    //             animate={{ x: 0, opacity: 1 }}
+    //             transition={{ duration: 0.5, delay: 0.5 }}
+    //             className="w-[70%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
+    //           >
+    //             <span className="absolute -top-7 right-0">70%</span>
+    //           </motion.span>
+    //         </span>
+    //       </div>
+    //       <div className="overflow-x-hidden">
+    //         <p className="text-sm uppercase font-medium">Design</p>
+    //         <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
+    //         </span>
+    //       </div>
+    //     </div>
+    //   </div>
+    //   <div className="w-full lgl:w-1/2">
+    //     <div className="py-12 font-titleFont flex flex-col gap-4">
+    //       <p className="text-sm text-designColor tracking-[4px] uppercase">
+    //         Features
+    //       </p>
+    //       <h2 className="text-3xl md:text-4xl font-bold">Development Skill</h2>
+    //     </div>
+    //     <div className="flex flex-col gap-6">
+    //       <div className="overflow-x-hidden">
+    //         <p className="text-sm uppercase font-medium">React</p>
+    //         <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
+    //         </span>
+    //       </div>
+    //       <div className="overflow-x-hidden">
+    //         <p className="text-sm uppercase font-medium">HTML 5</p>
+    //         <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
+    //           <motion.span
+    //             initial={{ x: "-100%", opacity: 0 }}
+    //             animate={{ x: 0, opacity: 1 }}
+    //             transition={{ duration: 0.5, delay: 0.5 }}
+    //             className="w-[95%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
+    //           >
+    //             <span className="absolute -top-7 right-0">95%</span>
+    //           </motion.span>
+    //         </span>
+    //       </div>
+    //       <div className="overflow-x-hidden">
+    //         <p className="text-sm uppercase font-medium">CSS3</p>
+    //         <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
+    //         </span>
+    //       </div>
+    //       <div className="overflow-x-hidden">
+    //         <p className="text-sm uppercase font-medium">JAVASCRIPT</p>
+    //         <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
+    //         </span>
+    //       </div>
+    //       <div className="overflow-x-hidden">
+    //         <p className="text-sm uppercase font-medium">SOFTWARE</p>
+    //         <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
+    //         </span>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </motion.div>
+    );
 };
 export default Skills;
